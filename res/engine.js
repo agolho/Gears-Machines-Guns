e9cec3928ee9cfe07923c828f4848298494403d8
Yusuf Bektas <agolho@gmail.com>
2017-03-22 00:16:27 +0300

    Save functionality

--- res/engine.js
+++ res/engine.js
@@ -1,9 +1,23 @@
 var gears,machines,guns,files,lathes = new Number();
-gears=0,machines=0,guns = 0,files=0,lathes=0,cncs=0,waterjets=0;
+var Data = localStorage.getItem("simpleData");
 //These all were dutluk, next time they will be using something delicious nomoomonomonom!
+//Kookiies
+if (Data == null){
+
+  gears=0,machines=0,guns = 0,files=0,lathes=0,cncs=0,waterjets=0;
+  localStorage.setItem("simpleData",[[gears,machines,guns,files,lathes,cncs,waterjets]]);
+} else {
+  var arrayed = Data.split(',');
+  gears=Math.round(arrayed[0]),machines=Math.round(arrayed[1]),guns = Math.round(arrayed[2]),files=Math.round(arrayed[3]),lathes=Math.round(arrayed[4]),cncs=Math.round(arrayed[5]),waterjets=Math.round(arrayed[6]);
+}
 //Initializing
 setInterval(numbermaster5k, 250);
 setInterval(purchaseable, 250);
+setInterval(autoSave, 1000);
+//Autosave
+function autoSave(){
+  localStorage.setItem("simpleData",[Math.round(gears),Math.round(machines),Math.round(guns),Math.round(files),Math.round(lathes),Math.round(cncs),Math.round(waterjets)]);
+}
 //Manual Churning
 $("#addonebtn").click(function(e){
   e.preventDefault();
@@ -13,12 +27,25 @@ function addone(){
   gears++;
   writer();
 }
+//Price Calculator
+function priceCalc(value){
+  return Math.round(15*(Math.pow(1.15,value)));
+}
+function priceLathes(value){
+  return Math.round(100*(Math.pow(1.15,value)));
+}
+function priceCnc(value){
+  return Math.round(1100*(Math.pow(1.15,value)));
+}
+function priceWjet(value){
+  return Math.round(12000*(Math.pow(1.15,value)));
+}
 //Writer
 function writer(){
-  $("#main .simplefile").html('<kbd>10</kbd> Simple File <span class="badge">'+files+'</span>');
-  $("#main .metallathe").html('<kbd>100</kbd> Metal Lathe <span class="badge">'+lathes+'</span>');
-  $("#main .cnc").html('<kbd>1K</kbd> CNC <span class="badge">'+cncs+'</span>');
-  $("#main .waterjet").html('<kbd>100K</kbd> WaterJet <span class="badge">'+waterjets+'</span>');
+  $("#main .simplefile").html('<kbd>'+priceCalc(files)+'</kbd> Simple File <span class="badge">'+files+'</span>');
+  $("#main .metallathe").html('<kbd>'+priceLathes(lathes)+'</kbd> Metal Lathe <span class="badge">'+lathes+'</span>');
+  $("#main .cnc").html('<kbd>'+priceCnc(cncs)+'</kbd> CNC <span class="badge">'+cncs+'</span>');
+  $("#main .waterjet").html('<kbd>'+priceWjet(waterjets)+'</kbd> WaterJet <span class="badge">'+waterjets+'</span>');
   var rounded=Math.round(gears);
   if(rounded<999){
     banksy('GEARS',rounded);
@@ -37,25 +64,25 @@ var lathebutton = document.getElementById("metal-lathe");
 var cncbutton = document.getElementById("cnc");
 var wjetbutton = document.getElementById("water-jet");
 function purchaseable(){
-  if (gears>=10){
+  if (gears>=priceCalc(files)){
       filebutton.disabled = false;
       }
       else {
         filebutton.disabled = true;
       }
-  if (gears>=100){
+  if (gears>=priceLathes(lathes)){
       lathebutton.disabled = false;
       }
       else {
       lathebutton.disabled = true;
       }
-  if (gears>=1000){
+  if (gears>=priceCnc(cncs)){
       cncbutton.disabled = false;
       }
       else {
         cncbutton.disabled = true;
       }
-  if (gears>=100000){
+  if (gears>=priceWjet(waterjets)){
       wjetbutton.disabled = false;
       }
       else {
@@ -65,33 +92,33 @@ function purchaseable(){
 //Machine Purchase
 $("#simple-file").click(function(e){
   e.preventDefault();
-  if (gears>=10){
+  if (gears>=priceCalc(files)){
     files++;
-    gears=gears-10;
+    gears-=priceCalc(files-1);
   }
   writer();
 });
 $("#metal-lathe").click(function(e){
   e.preventDefault();
-  if (gears>=100){
+  if (gears>=priceLathes(lathes)){
     lathes++;
-    gears=gears-100;
+    gears-=priceLathes(lathes-1);
   }
   writer();
 });
 $("#cnc").click(function(e){
   e.preventDefault();
-  if (gears>=1000){
+  if (gears>=priceCnc(cncs)){
     cncs++;
-    gears=gears-1000;
+    gears-=priceCnc(cncs-1);
   }
   writer();
 });
 $("#water-jet").click(function(e){
   e.preventDefault();
-  if (gears>=100000){
+  if (gears>=priceWjet(waterjets)){
     waterjets++;
-    gears=gears-100000;
+    gears-=priceWjet(waterjets-1);
   }
   writer();
 });
@@ -116,14 +143,14 @@ function numbermaster5k(){
 }
 //Machines Producing
 function filing(){
-  gears=gears+0.125;
+  gears+=0.025;
 }
 function lathing(){
-  gears=gears+2;
+  gears+=0.25;
 }
 function cncing(){
-  gears=gears+10;
+  gears+=2;
 }
 function waterjetting(){
-  gears=gears+20;
+  gears+=12;
 }
